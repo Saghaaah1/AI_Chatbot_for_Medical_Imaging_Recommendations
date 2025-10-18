7c7a7acf6ef47893d0332f129dedd56408fa03da
SARA_EL_BARI <sara.el-bari@imt-atlantique.net>
2025-10-19 01:17:52 +0200

    Initial push: project structure and ingestion module

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from ai-chatbot-for-medical-imaging-recommendations!")
+# main.py
+"""
+AI Chatbot for Medical Imaging Recommendations (RAG Project)
+-----------------------------------------------------------
+Author: Sara El Bari
+Description: Clinical decision-support chatbot that recommends imaging
+based on French medical guidelines (ADERIM, SFR, HAS).
+"""
 
+from src.ingestion.create_index import create_index
+from src.retrieval.query_engine import query_engine
 
 if __name__ == "__main__":
-    main()
+    print("===RAG Medical Chatbot ===")
+    print("1. Building or loading the vector database...")
+    create_index()
+
+    print("2. Asking a test clinical question...")
+    response = query_engine("Patiente de 45 ans, dyspnée aiguë, douleur thoracique pleuritique.")
+    print("\nResponse:\n", response)
+
